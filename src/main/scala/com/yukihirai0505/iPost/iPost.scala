22951af605bdfe7cdc224762639c464ba46a8c9a
Yuki Hirai <yukihirai0505@gmail.com>
2017-05-29 19:53:38 +0900

    Add postImage

--- src/main/scala/com/yukihirai0505/iPost/iPost.scala
+++ src/main/scala/com/yukihirai0505/iPost/iPost.scala
@@ -1,21 +1,28 @@
 package com.yukihirai0505.iPost
 
+import java.io.File
 import java.net.URLEncoder
 import javax.crypto.Mac
 import javax.crypto.spec.SecretKeySpec
 
-import com.yukihirai0505.com.scala.Request
-import com.yukihirai0505.com.scala.constants.Verbs
-import dispatch.{Req, url}
+import play.api.libs.json.Reads
 
-import scala.concurrent.Future
+import com.ning.http.client.cookie.Cookie
+import com.ning.http.client.multipart.{FilePart, StringPart}
+import com.yukihirai0505.com.scala.constants.Verbs
+import dispatch.{Future, Http, Req, url}
+
+import scala.collection.JavaConversions._
+import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
 
 object iPost extends App {
 
   val (username, password) = (sys.env("INSTAGRAM_USERNAME"), sys.env("INSTAGRAM_PASSWORD"))
+  var cookies: List[Cookie] = List.empty
 
-  def signIn(): Unit = {
+  def signIn(): Future[Option[String]] = {
     val loginUrl = "https://instagram.com/api/v1/accounts/login/"
     val uuid = java.util.UUID.randomUUID.toString
     val deviceId = s"android-$uuid"
@@ -26,24 +33,42 @@ object iPost extends App {
     val effectiveRequest = request
       .addHeader("User-Agent", "Instagram 4.0.0 Android (10/3.3.3; 240; 480x320; samsung; GT-I9220; GT-I9220; smdkc210; en_US)")
       .setContentType("application/x-www-form-urlencoded", "UTF-8")
-    Request.send[String](effectiveRequest).flatMap { _ =>
-      Future successful "hoge"
-    }
+    send[String](effectiveRequest)
   }
 
-  def postImage(): Unit = {
-
+  def postImage(postImage: File): Unit = {
+    val uploadUrl = "https://instagram.com/api/v1/media/upload/"
+    val request: Req = url(uploadUrl)
+      .setMethod(Verbs.POST.label)
+      .addHeader("User-Agent", "Instagram 4.0.0 Android (10/3.3.3; 240; 480x320; samsung; GT-I9220; GT-I9220; smdkc210; en_US)")
+      .addBodyPart(new FilePart("photo", postImage))
+      .addBodyPart(new StringPart("device_timestamp", (System.currentTimeMillis / 1000).toString))
+    def addCookies(cookies: List[Cookie], req: Req): Req = {
+      if (cookies.isEmpty) req
+      else addCookies(cookies.tail, req.addCookie(cookies.head))
+    }
+    val effectiveRequest = addCookies(cookies, request)
+    send[String](effectiveRequest)
   }
 
   def configureImage(): Unit = {
-
   }
 
-  def hashHmac(s: String, secret:String): String = {
+  def hashHmac(s: String, secret: String): String = {
     val sha256_HMAC = Mac.getInstance("HmacSHA256")
     sha256_HMAC.init(new SecretKeySpec(secret.getBytes("UTF-8"), "HmacSHA256"))
-    sha256_HMAC.doFinal(s.getBytes("UTF-8")).map(char=>f"$char%02x").mkString
+    sha256_HMAC.doFinal(s.getBytes("UTF-8")).map(char => f"$char%02x").mkString
   }
 
-  signIn()
+  def send[T](request: Req)(implicit r: Reads[T]): Future[Option[T]] = {
+    Http(request).map { resp =>
+      val response = resp.getResponseBody
+      println(response)
+      cookies = resp.getCookies.toList
+      None
+    }
+  }
+
+  Await.result(signIn(), Duration.Inf)
+  postImage(new File("yukihirai.jpeg"))
 }
