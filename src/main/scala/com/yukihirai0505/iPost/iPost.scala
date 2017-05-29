e66dbf47c085c14cb50285a3416abd6762d87ae1
Yuki Hirai <yukihirai0505@gmail.com>
2017-05-29 19:58:40 +0900

    WIP: Add configureImage

--- src/main/scala/com/yukihirai0505/iPost/iPost.scala
+++ src/main/scala/com/yukihirai0505/iPost/iPost.scala
@@ -21,11 +21,11 @@ object iPost extends App {
 
   val (username, password) = (sys.env("INSTAGRAM_USERNAME"), sys.env("INSTAGRAM_PASSWORD"))
   var cookies: List[Cookie] = List.empty
+  val uuid = java.util.UUID.randomUUID.toString
+  val deviceId = s"android-$uuid"
 
   def signIn(): Future[Option[String]] = {
     val loginUrl = "https://instagram.com/api/v1/accounts/login/"
-    val uuid = java.util.UUID.randomUUID.toString
-    val deviceId = s"android-$uuid"
     val params = s"""{"username":"$username","password":"$password","guid":"$uuid","device_id":"$deviceId"}"""
     val body = s"ig_sig_key_version=4&signed_body=${hashHmac(params, "b4a23f5e39b5929e0666ac5de94c89d1618a2916")}.${URLEncoder.encode(params, "UTF-8").replaceAll("\\+", "%20").replaceAll("\\-", "%2D")}"
     println(body)
@@ -42,7 +42,7 @@ object iPost extends App {
       .setMethod(Verbs.POST.label)
       .addHeader("User-Agent", "Instagram 4.0.0 Android (10/3.3.3; 240; 480x320; samsung; GT-I9220; GT-I9220; smdkc210; en_US)")
       .addBodyPart(new FilePart("photo", postImage))
-      .addBodyPart(new StringPart("device_timestamp", (System.currentTimeMillis / 1000).toString))
+      .addBodyPart(new StringPart("device_timestamp", timestamp))
     def addCookies(cookies: List[Cookie], req: Req): Req = {
       if (cookies.isEmpty) req
       else addCookies(cookies.tail, req.addCookie(cookies.head))
@@ -51,13 +51,20 @@ object iPost extends App {
     send[String](effectiveRequest)
   }
 
-  def configureImage(): Unit = {
+  def configureImage(mediaId: String, caption: String): Unit = {
+    val configureUrl = "https://instagram.com/api/v1/media/configure/"
+    val params = s"""{"guid":"$uuid","device_id":"$deviceId","device_timestamp":"$timestamp","media_id":"$mediaId","caption":"$caption","source_type":"5","filter_type":"0","extra":"{}"}"""
+    // TODO
   }
 
   def hashHmac(s: String, secret: String): String = {
     val sha256_HMAC = Mac.getInstance("HmacSHA256")
     sha256_HMAC.init(new SecretKeySpec(secret.getBytes("UTF-8"), "HmacSHA256"))
     sha256_HMAC.doFinal(s.getBytes("UTF-8")).map(char => f"$char%02x").mkString
+  }
+
+  def timestamp: String = {
+    (System.currentTimeMillis / 1000).toString
   }
 
   def send[T](request: Req)(implicit r: Reads[T]): Future[Option[T]] = {
