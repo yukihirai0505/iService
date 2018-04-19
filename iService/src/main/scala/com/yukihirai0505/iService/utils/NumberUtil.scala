2c9c1ce9ef0f1e3386f64c310f94cad876be1883
Yuki Hirai <yukihirai0505@gmail.com>
2018-04-19 17:00:28 +0900

    Update random int

--- iService/src/main/scala/com/yukihirai0505/iService/utils/NumberUtil.scala
+++ iService/src/main/scala/com/yukihirai0505/iService/utils/NumberUtil.scala
@@ -1,7 +1,7 @@
 package com.yukihirai0505.iService.utils
 
 object NumberUtil {
-  def getRandomInt(min: Int = 2000, max: Int = 10000): Long = {
+  def getRandomInt(min: Int = 5000, max: Int = 20000): Long = {
     (Math.floor(Math.random() * (max - min + 1)) + min).toLong
   }
 }
